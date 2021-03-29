c5650e89bbc8a1953c9b794abc742c552de8cd12
Abenaesha <Abenaesha@github.com>
2021-03-29 13:05:17 +0100

    components layout - the biz that gets the Git

---
+++ gitbiz/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ gitbiz/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Gitbiz</h1>
+      <h2>The biz that gets the Git</h2>
+    </header>
+  )
+}
+
+export default Header;
